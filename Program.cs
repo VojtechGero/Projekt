3dc681c86c6828431a53228f8a20780c04fd7c35
VojtechGero <vojtech.gero@tul.cz>
2024-03-11 11:43:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿
+
+/*
+DateOnly getDate(string date)
+{
+    string[] parts = date.Split(".");
+    DateOnly dateOnly = new DateOnly(int.Parse(parts[2]), int.Parse(parts[1]), int.Parse(parts[0]));
+    return dateOnly;
+}
+
+double[] aggregate(double[] values,int num)
+{
+    var doubles=new List<double>();
+    int overhang = values.Length % num;
+    double sum;
+    for (int i = 0; i < values.Length - overhang; i += num)
+    {
+        sum = 0;
+        for(int j = i; j < i + num; j++)
+        {
+            sum += values[j];
+        }
+        doubles.Add(sum/num);
+    }
+    sum = 0;
+    for (int i = values.Length - overhang;i< values.Length; i++)
+    {
+        sum+= values[i];
+    }
+    doubles.Add(sum / overhang);
+    return doubles.ToArray();
+}
+
+string path = "C:\\Users\\vojte\\OneDrive\\Plocha\\data.csv";
+string[] lines=File.ReadAllLines(path);
+List<Value> values = new List<Value>();
+for(int i=3; i<lines.Length; i++)
+{
+    string[] parts = lines[i].Split(";",options:StringSplitOptions.RemoveEmptyEntries);
+    values.Add(new Value(getDate(parts[0]), double.Parse(parts[1]), double.Parse(parts[2])));
+}
+double sum = 0;
+foreach(Value value in values)
+{
+    sum += value.zatizeni;
+    Console.WriteLine($"{value.zatizeni}");
+}
+Console.WriteLine($"\nPrůměr: {sum/values.Count}\n");
+sum = 0;
+double[] d = aggregate(values.Select(x => x.zatizeni).ToArray(), 7);
+foreach (var i in d)
+{
+    sum += i;
+    Console.WriteLine(i);
+}
+Console.WriteLine("\n"+sum / d.Length);
+
+class Value
+{
+    public DateOnly date { get; set; }
+    public double zatizeniCerpani { get; set; }
+    public double zatizeni {  get; set; }
+
+    public Value(DateOnly date, double zatizeniCerpani, double zatizeni)
+    {
+        this.date = date;
+        this.zatizeniCerpani = zatizeniCerpani;
+        this.zatizeni = zatizeni;
+    }
+}
+*/
+
+
+
+using Microsoft.ML;
+using Microsoft.ML.Transforms.TimeSeries;
+using Semestral;
+
+
+
+string trainPath = "..\\..\\..\\train.txt";
+string testPath = "..\\..\\..\\test.txt";
+
+var context=new MLContext();
+var data = context.Data.LoadFromEnumerable(Utilities.getData(trainPath));
+List<Value> test = Utilities.getData(testPath);
+var pipeline = context.Forecasting.ForecastBySsa(
+    "Forecast",
+    nameof(Value.zatizeniCerpani),
+    windowSize: 24*7,
+    seriesLength: 24*30,
+    trainSize: File.ReadAllLines(trainPath).Length - 1,
+    horizon: File.ReadAllLines(testPath).Length - 1
+    );
+var model = pipeline.Fit(data);
+var forecastingEngine=model.CreateTimeSeriesEngine<Value,ValueForcast>(context);
+var forecast = forecastingEngine.Predict();
+
+Utilities.evaluate(test, forecast.Forecast.ToList());
+
+
+internal class ValueForcast
+{
+    public float[] Forecast {  get; set; }
+}
+
+internal class Value
+{
+    public DateTime date { get; set; }
+    public float zatizeniCerpani { get; set; }
+    //[LoadColumn(2)]
+    //public double zatizeni { get; set; }
+    public Value(DateTime date, float zatizeniCerpani)
+    {
+        this.date = date;
+        this.zatizeniCerpani = zatizeniCerpani;
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semestral
+{
+    internal static class Utilities
+    {
+        internal static List<Value> getData(string filePath)
+        {
+            List<Value> data = new List<Value>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                if (!char.IsLetter(line[0]))
+                {
+                    string[] vals = line.Split(';');
+                    DateTime datum = DateTime.Parse(vals[0]);
+                    float zatizeniSCerpanim = float.Parse(vals[1]);
+                    data.Add(new Value(datum, zatizeniSCerpanim));
+                }
+            }
+            return data;
+        }
+
+        internal static List<float> errors(List<float> actualData, List<float> forecastData)
+        {
+            List<float> errors = new List<float>();
+            for (int i = 0; i < int.Min(actualData.Count, forecastData.Count); i++)
+            {
+                errors.Add(actualData[i] - forecastData[i]);
+            }
+            return errors;
+        }
+
+        internal static double MeanAbsoluteError(List<float> error)
+        {
+            return error.Average(x=>Math.Abs(x));
+        }
+
+        internal static double RootMeanSquare(List<float> error)
+        {
+            return Math.Sqrt(error.Average(x=>x*x));
+        }
+
+        internal static void evaluate(List<Value> TestData, List<float> actual)
+        {
+            List<float> forecast = TestData.Select(s => s.zatizeniCerpani).ToList();
+            var metrics = errors(actual, forecast);
+            var MAE = MeanAbsoluteError(metrics);
+            var RMSE = RootMeanSquare(metrics);
+            Console.WriteLine("Evaluation Metrics");
+            Console.WriteLine("---------------------");
+            Console.WriteLine($"Mean Absolute Error: {MAE:F3}");
+            Console.WriteLine($"Root Mean Squared Error: {RMSE:F3}\n");
+        }
+
+    }
+}
